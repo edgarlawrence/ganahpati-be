ac46e02c097924c8917c95e98311e7d2086ed1d5
edgarlawrence <lawofueki91@hotmail.com>
2024-12-11 02:50:43 +0700

    coba

--- src/app.js
+++ src/app.js
@@ -1,22 +1,3 @@
-// app.js
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
-require('dotenv').config();
-const taxiDataRoutes = require('./routes/TaxiRoutes');
-const TestRoutes = require('./routes/TestRoutes')
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-const API_URL = process.env.API_URL;
-
-// Middleware
-app.use(cors({
-    credentials: true,
-    origin: ["http://localhost:3000", "http://www.localhost:3000"]
-  }));
-app.use(express.json());
-
-// Declare the routes
-app.use('/', TestRoutes);
-app.use('/api', taxiDataRoutes);
+const { createServer } = require('@vercel/node');
+const app = require('../index');
+module.exports = createServer(app);
