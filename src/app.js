092a86f1c8dec4a11b85806f8b4ba327323aa699
edgarlawrence <lawofueki91@hotmail.com>
2024-12-11 01:03:01 +0700

    reconfigure vercel

---
+++ src/app.js
@@ -0,0 +1,27 @@
+// app.js
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+require('dotenv').config();
+const taxiDataRoutes = require('./src/routes/TaxiRoutes');
+const TestRoutes = require('./src/routes/TestRoutes')
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const API_URL = process.env.API_URL;
+
+// Middleware
+app.use(cors({
+    credentials: true,
+    origin: ["http://localhost:3000", "http://www.localhost:3000"]
+  }));
+app.use(express.json());
+
+// Declare the routes
+app.use('/', TestRoutes);
+app.use('/api', taxiDataRoutes);
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
