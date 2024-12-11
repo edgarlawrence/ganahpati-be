a3327d972c6a68c50660110412544e943faf0802
edgarlawrence <lawofueki91@hotmail.com>
2024-12-11 19:01:05 +0700

    test lagi

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ const app = express();
 // Middleware
 app.use(cors({
     credentials: true,
-    origin: ["http://localhost:3000", "http://www.localhost:3000"]
+    origin: ["http://localhost:3000", "http://www.localhost:3000", "https://your-frontend.vercel.app"]
 }));
 app.use(express.json());
 
