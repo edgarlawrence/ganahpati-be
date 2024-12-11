2a434c2b7f8f8411432153ed4dbf1dac3eb58afd
edgarlawrence <lawofueki91@hotmail.com>
2024-12-11 19:38:24 +0700

    add lagi

--- api/index.js
+++ api/index.js
@@ -8,10 +8,26 @@ const TestRoutes = require('../src/routes/TestRoutes');
 const app = express();
 
 // Middleware
-app.use(cors({
-    credentials: true,
-    origin: ["https://ganahpati-fe.vercel.app/", "https://ganahpati-nznp2igww-edgarlawrences-projects.vercel.app/"]
-}));
+// app.use(cors({
+//     credentials: true,
+//     origin: ["http://localhost:3000", "http://www.localhost:3000"]
+// }));
+
+
+const allowedOrigins = [
+    'https://ganahpati-nznp2igww-edgarlawrences-projects.vercel.app', // Frontend domain
+  ];
+  
+  app.use(cors({
+      origin: (origin, callback) => {
+          if (!origin || allowedOrigins.includes(origin)) {
+              callback(null, true);
+          } else {
+              callback(new Error('Not allowed by CORS'));
+          }
+      },
+      credentials: true,
+  }));
 app.use(express.json());
 
 // Declare the routes
